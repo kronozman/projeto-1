b9d8d18704d40927ee3775fbe4475d28b6e98dda
Paulo Ramos <kronozman@gmail.com>
2024-03-18 16:11:56 +0000

    commit inicial

---
+++ src/components/accordian/index.jsx
@@ -0,0 +1,35 @@
+//single selection
+//multiple selection
+
+import React, { useState } from "react";
+import data from "./data";
+
+export default function Accordian() {
+  const [selected, setSelected] = useState(null);
+
+  function handleSingleSelection(getCurrentId) {
+    console.log(getCurrentId);
+  }
+
+  return (
+    <div className="wrapper">
+      <div className="accordian">
+        {data && data.length > 0 ? (
+          data.map((dataItem) => (
+            <div className="item">
+              <div
+                onClick={() => handleSingleSelection(dataItem.id)}
+                className="title"
+              >
+                <h3>{dataItem.question}</h3>
+                <span>➕</span>
+              </div>
+            </div>
+          ))
+        ) : (
+          <div>No data found!</div>
+        )}
+      </div>
+    </div>
+  );
+}
