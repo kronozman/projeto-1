f5ad492c02ac9f49e7d758a1e88941dd63d20292
Paulo Ramos <kronozman@gmail.com>
2024-03-21 16:08:37 +0000

    21 de marco no trabalho

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
-import Accordian from "./components/accordian";
-import RandomColor from "./components/random-color";
+// import Accordian from "./components/accordian";
+// import RandomColor from "./components/random-color";
+// import StarRating from "./components/star-rating";
+import ImageSlider from "./components/image-slider";
 
 function App() {
   return (
@@ -9,7 +11,17 @@ function App() {
       {/* <Accordian /> */}
 
       {/* Random Color Component */}
-      <RandomColor />
+      {/* <RandomColor /> */}
+
+      {/* Star-Rating Component */}
+      {/* <StarRating noOfStars={10} /> */}
+
+      {/* Image-Slider Component */}
+      <ImageSlider
+        url={"https://picsum.photos/v2/list"}
+        limit={"10"}
+        page={"1"}
+      />
     </div>
   );
 }
---
+++ src/components/image-slider/index.jsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react";
+import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from "react-icons/bs";
+
+export default function ImageSlider({ url, limit = 5, page = 1 }) {
+  const [images, setImages] = useState([]);
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const [errorMsg, setErrorMsg] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  async function fetchImages(getUrl) {
+    try {
+      const response = await fetch(`${getUrl}?page=${page}&limit=${limit}`);
+      const data = await response.json();
+
+      setLoading(true);
+
+      if (data) {
+        setImages(data);
+        setLoading(false);
+      }
+    } catch (e) {
+      setErrorMsg(e.message);
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    if (url !== "") fetchImages(url);
+  }, [url]);
+
+  if (loading) {
+    return <div>Loading data ! Please wait</div>;
+  }
+
+  if (errorMsg !== null) {
+    return <div>Error occured ! {errorMsg}</div>;
+  }
+
+  return (
+    <div className="container">
+      <BsArrowLeftCircleFill className="arrow arrow-left" />
+    </div>
+  );
+}
